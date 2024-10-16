1ecac41a58efd5dfa4706b7f1b0bfa2bf735d59e
Nikola <140314923+NikoStano@users.noreply.github.com>
2024-10-17 01:27:10 +0200

    Update ft_swap.c
    add main

--- C01/ex02/ft_swap.c
+++ C01/ex02/ft_swap.c
@@ -13,9 +13,39 @@
 void	ft_swap(int *a, int *b)
 
 {
-	int	c;
+	int	temp;
 
-	c = *a;
+	temp = *a;
 	*a = *b;
-	*b = c;
+	*b = temp;
 }
+
+// void    ft_putchar(char c)
+// {
+//     write(1, &c, 1);
+// }
+// void    ft_putint(int nb)
+// {
+//     if (nb < 0)
+//     {
+//         nb = -nb;
+//         ft_putchar('-');
+//     }
+//     if (nb / 10)
+//         ft_putint(nb / 10);
+//     ft_putchar(nb % 10 + 48);
+// }
+// int main(void)
+// {
+//     int a = 42;
+//     int b = 24;
+//     ft_putint(a);
+//     ft_putchar('\n');
+//     ft_putint(b);
+//     ft_putchar('\n');
+//     ft_swap(&a, &b);
+//     ft_putint(a);
+//     ft_putchar('\n');
+//     ft_putint(b);
+//     ft_putchar('\n');
+// }
