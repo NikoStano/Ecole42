88862c6d92be93acd5b41019dd24db260c77151e
Nikola <140314923+NikoStano@users.noreply.github.com>
2024-10-17 01:26:12 +0200

    Update ft_ultimate_ft.c
    ajout main

--- C01/ex01/ft_ultimate_ft.c
+++ C01/ex01/ft_ultimate_ft.c
@@ -14,3 +14,41 @@ void	ft_ultimate_ft(int *********nbr)
 {
 	*********nbr = 42;
 }
+
+// void    ft_putchar(char c)
+// {
+//     write(1, &c, 1);
+// }
+// void    ft_putint(int i)
+// {
+//     if (i / 10)
+//         ft_putint(i / 10);
+//     ft_putchar(i % 10 + 48);
+// }
+// int main(void)
+// {
+//     int v;
+//     int *********nbr; 
+//     int ********nbr1;
+//     int *******nbr2;
+//     int ******nbr3;
+//     int *****nbr4;
+//     int ****nbr5;
+//     int ***nbr6;
+//     int **nbr7;
+//     int *nbr8;
+
+//     nbr8 = &v; 
+//     nbr7 = &nbr8; 
+//     nbr6 = &nbr7; 
+//     nbr5 = &nbr6; 
+//     nbr4 = &nbr5; 
+//     nbr3 = &nbr4;
+//     nbr2 = &nbr3;
+//     nbr1 = &nbr2;
+//     nbr = &nbr1;
+//     ft_ultimate_ft(nbr);
+//     ft_putint(v);
+//     ft_putchar('\n');
+//     return (0);
+// }
