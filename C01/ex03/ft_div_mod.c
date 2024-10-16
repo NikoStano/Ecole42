772a51a00772c104a06dfb551da2e1c6cec96641
Nikola <140314923+NikoStano@users.noreply.github.com>
2024-10-17 01:31:52 +0200

    Update ft_div_mod.c
    add main

--- C01/ex03/ft_div_mod.c
+++ C01/ex03/ft_div_mod.c
@@ -17,3 +17,31 @@ void	ft_div_mod(int a, int b, int *div, int *mod)
 	*div = a / b;
 	*mod = a % b;
 }
+
+// void    ft_putchar(char c)
+// {
+//     write(1, &c, 1);
+// }
+// void    ft_putint(int nb)
+// {
+//     if (nb < 0)
+//     {
+//         nb = -nb;
+//         ft_putchar('-');
+//     }
+//     if (nb / 10)
+//         ft_putint(nb / 10);
+//     ft_putchar(nb % 10 + 48);
+// }
+// int main(void)
+// {
+//     int div;
+//     int mod;
+//     int a = 21;
+//     int b = 2;
+//     ft_div_mod(a, b, &div, &mod);
+//     ft_putint(div);
+//     ft_putchar('\n');
+//     ft_putint(mod);
+//     ft_putchar('\n');
+// }
