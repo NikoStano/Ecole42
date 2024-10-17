36314f61ff6934803c24eb12cc216b082c97a1ef
Nikola <140314923+NikoStano@users.noreply.github.com>
2024-10-17 02:59:09 +0200

    Update ft_putstr.c

--- C04/ex01/ft_putstr.c
+++ C04/ex01/ft_putstr.c
@@ -23,3 +23,18 @@ void	ft_putstr(char *str)
 		i++;
 	}
 }
+
+// int	main(int ac, char **av)
+// {
+//     int	i = 1;
+//     if (ac > 1)
+//     {
+//         while (i < ac)
+//         {
+//             ft_putstr(av[i]);
+//             write(1, "\n", 1);
+//             i++;
+//         }
+//     }
+//     return (0);
+// }
