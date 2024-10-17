53b5f14d1bc7e3431d8fc6e0702e797957691270
Nikola <140314923+NikoStano@users.noreply.github.com>
2024-10-17 02:44:46 +0200

    Update ft_recursive_power.c

--- C05/ex03/ft_recursive_power.c
+++ C05/ex03/ft_recursive_power.c
@@ -18,9 +18,7 @@ int	ft_recursive_power(int nb, int power)
 		return (0);
 	if (power == 0)
 		return (1);
-	while (power > 0)
-		return (nb * ft_recursive_power (nb, power - 1));
-	return (0);
+	return (nb * ft_recursive_power (nb, power - 1));
 }
 
 // #include <stdio.h>
