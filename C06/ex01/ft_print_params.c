441c30379b8cea0a6801324c80e779e3a72e03d9
Nikola <140314923+NikoStano@users.noreply.github.com>
2024-10-17 02:00:32 +0200

    Update ft_print_params.c

--- C06/ex01/ft_print_params.c
+++ C06/ex01/ft_print_params.c
@@ -12,35 +12,14 @@
 
 #include <unistd.h>
 
-// void	ft_putstr(char *str)
-// {
-// 	int	i;
-
-// 	i = 0;
-// 	while (str[i])
-// 	{
-// 		write(1, &str[i], 1);
-// 		i++;
-// 	}
-// }
-
-// int	main(int argc, char *argv[])
-// {
-// 	argc = 1;
-// 	while (argc[argv])
-// 	{
-// 		ft_putstr(argv[argc]);
-// 		ft_putstr("\n");
-// 	argc++;
-// 	}
-// }
-
 int	main(int argc, char *argv[])
 {
 	int	i;
 	int	j;
 
 	i = 1;
+	if (argc < 1)
+		write(1, "\n", 1);
 	while (i < argc)
 	{
 		j = 0;
