34b8eb89b5f3baa947e86eb2fc2757bf8b04ae7b
Nikola <140314923+NikoStano@users.noreply.github.com>
2024-10-17 02:40:47 +0200

    Update ft_recursive_factorial.c

--- C05/ex01/ft_recursive_factorial.c
+++ C05/ex01/ft_recursive_factorial.c
@@ -21,10 +21,13 @@ int	ft_recursive_factorial(int nb)
 	return (nb * ft_recursive_factorial (nb - 1));
 }
 
-// #include <stdio.h>
+/*
+#include <stdlib.h>
+#include <stdio.h>
 
-// int	main()
-// {
-// 	printf("Result : %d\n", ft_recursive_factorial(5));
-// 	return (0);
-// }
+int	main(int ac, char **av)
+{
+	(void)ac;
+	printf("Result : %d\n", ft_recursive_factorial(atoi(av[1])));
+	return (0);
+}*/
