66160f12c93c30769a524c0321fef5555400a80c
Nikola <140314923+NikoStano@users.noreply.github.com>
2024-10-17 02:24:09 +0200

    Update ft_putnbr.c
    add main

--- C04/ex02/ft_putnbr.c
+++ C04/ex02/ft_putnbr.c
@@ -12,26 +12,36 @@
 
 #include <unistd.h>
 
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
 void	ft_putnbr(int nb)
 {
-	int		i;
 	long	n;
-
-	i = 0;
+	
 	n = nb;
 	if (n < 0)
 	{
 		n = -n;
-		write(1, "-", 1);
+		ft_putchar("-");
 	}
 	if (n / 10)
 		ft_putnbr(n / 10);
-	i = n % 10 + 48;
-	write(1, &i, 1);
+	ft_putchar(n % 10 + 48);
 }
 
-// int	main(void)
+// int main(int ac, char **av)
 // {
-// 	ft_putnbr(12);
-// 	return (0);
+//     if (ac > 1 && ac < 3)
+//     {
+//         ft_putnbr(atoi(av[1]));
+//         ft_putchar('\n');
+//     }
+//     else
+//     {
+//         return (printf("1 arg !\n"));
+//     }
+//     return (0);
 // }
