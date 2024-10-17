ab5a50d035fdde75dff2ef8b69e906ef96ab90fd
Nikola <140314923+NikoStano@users.noreply.github.com>
2024-10-17 03:08:01 +0200

    Update ft_putchar.c

--- C00/ex00/ft_putchar.c
+++ C00/ex00/ft_putchar.c
@@ -11,15 +11,16 @@
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdlib.h>
 
 void	ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
 
-/*int		main()
+/*int		main(void)
 {
-	ft_putchar('A');
+	ft_putchar('a');
 	return(0);
 }
 */
