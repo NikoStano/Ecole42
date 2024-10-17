e86fb3f8cca115c5353771b311df63852310ad02
Nikola <140314923+NikoStano@users.noreply.github.com>
2024-10-17 02:56:45 +0200

    Update ft_strlen.c

--- C04/ex00/ft_strlen.c
+++ C04/ex00/ft_strlen.c
@@ -11,6 +11,7 @@
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdlib.h>
 
 int	ft_strlen(char *str)
 {
@@ -18,8 +19,32 @@ int	ft_strlen(char *str)
 
 	i = 0;
 	while (str[i])
-	{
 		i++;
-	}
 	return (i);
 }
+
+// void    ft_putchar(char c)
+// {
+//     write(1, &c, 1);
+// }
+// void    ft_putnbr(int nbr)
+// {
+//     if (nbr / 10)
+//         ft_putnbr(nbr / 10);
+//     ft_putchar(nbr % 10 + 48);
+    
+// }
+// int	main(int ac, char **av)
+// {
+//     int	i = 1;
+//     if (ac > 1)
+//     {
+//         while (i < ac)
+//         {
+//             ft_putnbr(ft_strlen(av[i]));
+//             ft_putchar('\n');
+//             i++;
+//         }
+//     }
+//     return (0);
+// }
